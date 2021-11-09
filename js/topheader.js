bec1e713aaaf0dff93190376d524153c95f7aa6f
Chirag Charania <chiragcharania@gmail.com>
2021-11-08 22:54:06 -0500

    Updated header

--- js/topheader.js
+++ js/topheader.js
@@ -13,21 +13,21 @@ let addTopHeader = function () {
                 '<div class="field field--name-body field--type-text-with-summary field--label-hidden field--item">'+
                     '<div id="logo">'+
                         '<a aria-label="University at Albany Home" href="https://www.albany.edu">'+
-                            '<img id="albany-logo-minerva" alt="UAlbany-Minerva" src="https://sdat-dev.github.io/resources/wiser/assets/images/top-menu/albany-logo-minerva.png">'+
-                            '<img id="albany-logo-mobile" alt="UAlbany" src="https://sdat-dev.github.io/resources/wiser/assets/images/ualbany-logo.png">'+
+                            '<img id="albany-logo-minerva" alt="UAlbany-Minerva" src="../resources/wiser/assets/images/top-menu/albany-logo-minerva.png">'+
+                            '<img id="albany-logo-mobile" alt="UAlbany" src="../resources/wiser/assets/images/ualbany-logo.png">'+
                         '</a>'+
-                            '<a aria-label="University of Connecticut Home" href="https://uconn.edu/">'+
-                            '<img id="albany-logo-minerva" alt="Uconn-Minerva" src="https://sdat-dev.github.io/resources/wiser/assets/images/top-menu/uconn-logo-minerva.png">'+
-                            '<img id="albany-logo-mobile" alt="UAlbany" src="https://sdat-dev.github.io/resources/wiser/assets/images/uconn-logo.png">'+
-                            '<img id="albany-logo-text" alt="WISER IUCRC" src="https://sdat-dev.github.io/resources/wiser/assets/images/top-menu/wiser-logo.png">'+
+                        '<a aria-label="University of Connecticut Home" href="https://uconn.edu/">'+
+                            '<img id="albany-logo-minerva" alt="Uconn-Minerva" src="../resources/wiser/assets/images/top-menu/uconn-logo-minerva.png">'+
+                            '<img id="albany-logo-mobile" alt="UAlbany" src="../resources/wiser/assets/images/uconn-logo.png">'+
+                            '<img id="albany-logo-text" alt="WISER IUCRC" src="../resources/wiser/assets/images/top-menu/wiser-logo.png">'+
                         '</a>'+
                     '</div>'+
-                    '<nav id="top-nav" aria-label="MyUAlbany and Give Links">'+
-                        '<ul>'+
-                            '<li><a title="Link to Faculty and Staff Portal" href="https://www.albany.edu/myualbany">MYUALBANY</a></li>'+
-                            '<li><a title="Donate to the University" href="https://www.alumni.albany.edu/s/1642/18-giving/landing.aspx?sid=1642&amp;gid=2&amp;pgid=2040&amp;appealcode=uahome">GIVE</a></li>'+
-                        '</ul>'+
-                    '</nav>'+
+                    // '<nav id="top-nav" aria-label="MyUAlbany and Give Links">'+
+                    //     '<ul>'+
+                    //         '<li><img id="albany-logo-minerva" alt="Uconn-Minerva" src="../resources/wiser/assets/images/top-menu/albany-logo-text.png"></li>'+
+                    //         '<li><img id="albany-logo-minerva" alt="Uconn-Minerva" src="../resources/wiser/assets/images/top-menu/uconn-logo-text-1.png"></li>'+
+                    //     '</ul>'+
+                    // '</nav>'+
                     '<nav id="main-nav" aria-label="Main Navigation Menu">'+
                         '<ul id="main-nav-top">'+
                             '<li>'+
@@ -100,7 +100,7 @@ let addTopHeader = function () {
                                         'class="headerSearchBox form-search glyphicon-search" /><input type="submit"'+
                                         'name="albanySearch" value="Search" title="Submit Search"'+
                                         'class="headerSearchBtn form-submit search-button btn btn-sm btn-success s3-m-3" />'+
-                                '</form>'+
+                                    '</form>'+
                                     '</div>'+
                                     '<li>'+
                                         '<div class="mobile-nav-toplink">'+
@@ -185,27 +185,27 @@ let addTopHeader = function () {
             '</div>'+
         '</div>'+
 
-        '<section id="block-albany-searchblockbutton"'+
-            'class="search-button block block-block-content block-block-contentf5fc99ed-9b0b-4fef-9f5a-708a4da0e836 clearfix"'+
-            'data-block-plugin-id="block_content:f5fc99ed-9b0b-4fef-9f5a-708a4da0e836">'+
-            '<div'+
-                'class="field field--name-body field--type-text-with-summary field--label-hidden field--item">'+
-                '<p><a href="/search-results">Search</a></p>'+
-            '</div>'+
-        '</section>'+
+        // '<section id="block-albany-searchblockbutton"'+
+        //     'class="search-button block block-block-content block-block-contentf5fc99ed-9b0b-4fef-9f5a-708a4da0e836 clearfix"'+
+        //     'data-block-plugin-id="block_content:f5fc99ed-9b0b-4fef-9f5a-708a4da0e836">'+
+        //     '<div'+
+        //         'class="field field--name-body field--type-text-with-summary field--label-hidden field--item">'+
+        //         '<p><a href="/search-results">Search</a></p>'+
+        //     '</div>'+
+        // '</section>'+
 
-        '<div class="search-block-form albany-utilities collapse small text-right search-processed">'+
-            '<form class="headerSearchForm form-type-search form-search" method="get" onsubmit="return albany_search(type.value, albany_URLEncode(keywords.value));">'+
-                '<select name="type" title="Select Search Type" class="headerSearchSelect" style="height:27px; display: none"><option selected="selected" value="GOOGLE">Albany Sites</option></select><label class="sr-only" for="global-search">Search UAlbany websites</label>'+
-                '<input type="text" name="keywords" id="global-search" value="" size="45" class="headerSearchBox form-search'+ 
-                'glyphicon-search"><input type="submit" name="albanySearch" value="Search" title="Submit Search" '+
-                'class="headerSearchBtn form-submit search-button btn btn-sm btn-success s3-m-3">'+
-            '</form>'+
-        '</div>'+
+        // '<div class="search-block-form albany-utilities collapse small text-right search-processed">'+
+        //     '<form class="headerSearchForm form-type-search form-search" method="get" onsubmit="return albany_search(type.value, albany_URLEncode(keywords.value));">'+
+        //         '<select name="type" title="Select Search Type" class="headerSearchSelect" style="height:27px; display: none"><option selected="selected" value="GOOGLE">Albany Sites</option></select><label class="sr-only" for="global-search">Search UAlbany websites</label>'+
+        //         '<input type="text" name="keywords" id="global-search" value="" size="45" class="headerSearchBox form-search'+ 
+        //         'glyphicon-search"><input type="submit" name="albanySearch" value="Search" title="Submit Search" '+
+        //         'class="headerSearchBtn form-submit search-button btn btn-sm btn-success s3-m-3">'+
+        //     '</form>'+
+        // '</div>'+
 
         '</div>'+
     '</div>'+
-'</div>'; 
+'</div>';
 sidemenu.innerHTML = content;
 }
 
